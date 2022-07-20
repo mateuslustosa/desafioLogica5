0695ddf4154b894c92714cd330958e488381cc20
mateuslustosa <mateuslustosa.o@gmail.com>
2022-07-20 14:06:16 -0300

    Desafio 5 completo!

---
+++ script.js
@@ -0,0 +1,26 @@
+//Crie um array que receba 5 itens e exiba no console.
+let frutas = ["Mamão", "Banana", "Morango", "Laranja", "Manga"];
+
+console.log(frutas);
+
+//Utilize um método para adicionar um nome no inicio o array
+frutas.unshift("Melão");
+console.log(frutas);
+
+//Utilize um método para remover o ultimo nome do array.
+frutas.pop( )
+console.log(frutas);
+
+//Ultimize um método para adionar dois nomes ao fim do array.
+frutas.push("Uva", "Maracuja");
+console.log(frutas);
+
+//Utilize metodo para remover o primeiro nome do array
+frutas.shift();
+console.log(frutas);
+
+//Utilize metodo para organizar em ordem crescente o seguinte array
+let numbers = [8,9,5,1,3,4,7,2,6];
+
+numbers.sort()
+console.log(numbers);
